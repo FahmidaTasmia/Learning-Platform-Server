f17fea1ab21449c0bbb13447047552269331a3c3
FahmidaTasmia <tasmia1070@gmail.com>
2022-10-26 11:03:31 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const categories = require('./data/categories.json');
+
+app.get('/',(req,res)=>{
+    res.send('pi Learning server is running')
+});
+
+app.get('/categories',(req,res)=>{
+    res.send(categories);
+})
+
+app.listen(port,()=>{
+    console.log(`Pi Learning running on Port, ${port}` );
+})
