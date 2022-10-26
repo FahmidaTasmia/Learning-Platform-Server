fa98fbd24fc79ccd2024d0c99097be862654f852
FahmidaTasmia <tasmia1070@gmail.com>
2022-10-26 12:45:43 +0600

    course Api data are added

--- index.js
+++ index.js
@@ -1,22 +1,42 @@
 const express = require('express');
-const cors = require('cors');
-
 const app = express();
-
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 
 const categories = require('./data/categories.json');
+const course = require('./data/course.json');
 
-app.get('/',(req,res)=>{
-    res.send('pi Learning server is running')
+app.get('/', (req, res) => {
+    res.send('PI learning is Running');
 });
 
-app.get('/categories',(req,res)=>{
-    res.send(categories);
+app.get('/course-categories', (req, res) => {
+    res.send(categories)
+});
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '08') {
+        res.send(course);
+    }
+    else {
+        const category_course = course.filter(n => n.category_id === id);
+        res.send(category_course);
+    }
 })
 
-app.listen(port,()=>{
-    console.log(`Pi Learning running on Port, ${port}` );
+app.get('/course', (req, res) =>{
+    res.send(course);
+});
+
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = course.find(n => n._id === id);
+    res.send(selectedCourse);
+});
+
+app.listen(port, () => {
+    console.log('PI learning Server running on port', port);
 })
